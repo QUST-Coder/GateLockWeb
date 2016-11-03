5ca0cd75943a5fe6f2e9c4a8b9024f761e4c9e3f
garry <plzwb@vip.qq.com>
2016-11-03 20:10:34 +0800

    bug fix

--- src/com/lock/servlet/OpenDoorServlet.java
+++ src/com/lock/servlet/OpenDoorServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.lock.service.WebOpenDoorService;
+import com.lock.util.StaticResource;
 /**
  * 开门Servelet
  * @author GarryChung
@@ -30,13 +31,18 @@ public class OpenDoorServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String studentId = request.getParameter("studentId");
 		String passWord = request.getParameter("passWord");
-		
-		WebOpenDoorService WebOpenDoorService = new WebOpenDoorService(studentId, passWord);
-		//发送开门请求，由Service负责验证用户
-		String openResult = WebOpenDoorService.open();
-		//返回开门结果
-		request.getSession().setAttribute("openResult", openResult);
-		response.sendRedirect(request.getContextPath() + "/open.jsp");
+		System.out.println(StaticResource.connect);
+		//判断门锁是否连接
+		if (StaticResource.connect.equals("未连接")) {
+			request.getSession().setAttribute("openResult", "门锁尚未连接请联系管理员！");
+			response.sendRedirect(request.getContextPath() + "/open.jsp");
+		}else {
+			WebOpenDoorService WebOpenDoorService = new WebOpenDoorService(studentId, passWord);
+			//发送开门请求，由Service负责验证用户
+			String openResult = WebOpenDoorService.open();
+			//返回开门结果
+			request.getSession().setAttribute("openResult", openResult);
+			response.sendRedirect(request.getContextPath() + "/open.jsp");
+		}
 	}
-
 }
